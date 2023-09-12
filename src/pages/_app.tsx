888c9a63313f25d5870f39eccd7a18e31e5ed5d6
Shaan <shaanalam369@gmail.com>
2023-09-12 13:02:26 +0530

    feat: add dark mode styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { ThemeProvider } from "@/components/theme-provider";
 
 import { api } from "@/utils/api";
 
@@ -12,7 +13,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 };
