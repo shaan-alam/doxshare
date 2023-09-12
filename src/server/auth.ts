dc4011588cfee98370296db8b963c64818132230
Shaan <shaanalam369@gmail.com>
2023-09-12 12:55:06 +0530

    feat: :sparkles: add github provider
    added github provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,6 +6,7 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 
 import { env } from "@/env.mjs";
 import { db } from "@/server/db";
@@ -51,6 +52,10 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
